7a73fdfee18f2cedbb663ed8541a74b8f838b27b
Hesham El-Sawaf <heshamlsawaf@gmail.com>
2016-10-29 23:30:31 +0200

    Paint now opens in full screen

--- src/view/main/HomeScene.java
+++ src/view/main/HomeScene.java
@@ -8,7 +8,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import util.HomeConstants;
+import javafx.stage.Screen;
 
 public class HomeScene extends Scene {
 
@@ -30,7 +30,8 @@ public class HomeScene extends Scene {
 
 
   public HomeScene(PaintController paintController, Home home) {
-    super(new BorderPane(), HomeConstants.HOME_WIDTH, HomeConstants.HOME_HEIGHT);
+    super(new BorderPane(), Screen.getPrimary().getVisualBounds().getWidth(),
+        Screen.getPrimary().getVisualBounds().getHeight());
 
     this.paintController = paintController;
     this.home = home;
@@ -56,13 +57,15 @@ public class HomeScene extends Scene {
     scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
     scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
     scrollPane.setContent(stackPane);
-    
-    stackPane.minWidthProperty().bind(Bindings.createDoubleBinding(() -> scrollPane.getViewportBounds().getWidth(), scrollPane.viewportBoundsProperty()));
-    stackPane.minHeightProperty().bind(Bindings.createDoubleBinding(() -> scrollPane.getViewportBounds().getHeight(), scrollPane.viewportBoundsProperty()));
-    
+
+    stackPane.minWidthProperty().bind(Bindings.createDoubleBinding(
+        () -> scrollPane.getViewportBounds().getWidth(), scrollPane.viewportBoundsProperty()));
+    stackPane.minHeightProperty().bind(Bindings.createDoubleBinding(
+        () -> scrollPane.getViewportBounds().getHeight(), scrollPane.viewportBoundsProperty()));
+
     this.borderPane.setCenter(scrollPane);
-    
-    
+
+
     this.setRoot(this.borderPane);
 
   }
